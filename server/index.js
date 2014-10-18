425209304173d5a690f2df5de13f0d06dd0ecf92
Rub21 <rub2106@gmail.com>
2014-10-17 23:31:46 -0400

    json

--- server/index.js
+++ server/index.js
@@ -2,42 +2,78 @@ var express = require('express');
 var cors = require('cors');
 var fs = require('fs');
 var path = require('path');
+var fs = require('fs');
+var readline = require('readline');
 
 var app = express();
 app.use(cors());
 
 console.log("http://localhost:3021/data");
 
+var objs = function() {
+	return {
+		values: [],
+		key: null,
+		color: "#ff7f0e"
+	};
+};
+
+
+
 app.get('/data/:date', function(req, res) {
 	var date = req.params.date;
-	var json = {
-		"type": "FeatureCollection",
-		"features": []
-	};
+	var array_json = [];
+	var users = [];
+
+
+
 	console.log(date);
-	var filePath = path.join(__dirname, '2014-10-15.csv');
-	fs.readFile(filePath, {
-		encoding: 'utf-8'
-	}, function(err, data) {
-		if (!err) {
-			console.log('received data: ' + data);
 
-			/*response.writeHead(200, {
-				'Content-Type': 'text/html'
-			});
-			response.write(data);
-			response.end();*/
-
-
-		} else {
-			console.log(err);
-		}
-
+	var rd = readline.createInterface({
+		input: fs.createReadStream('2014-10-15.csv'),
+		output: process.stdout,
+		terminal: false
 	});
 
 
+	var bandera = true;
+	var array_objs = [];
 
-	res.json(json);
+
+	rd.on('line', function(line) {
+		if (bandera) {
+			var users = line.split(',');
+			for (var i = 1; i < users.length; i++) {
+				var user = new objs();
+				user.key = users[i];
+				array_objs.push(user)
+
+			};
+			bandera = false;
+		} else {
+
+
+			var data = line.split(',');
+			for (var i = 1; i < data.length - 1; i++) {
+				array_objs[i].values.push({
+					x: data[0],
+					y: data[i]
+				});
+
+			};
+
+		}
+
+	}).on('close', function() {
+console.log(array_objs);
+		res.json(array_objs);
+  //process.exit(0);
+});
+
+
+
+
+
 
 
 });
