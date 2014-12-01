7a6e7f586ad149f812f4361f982b6f1f33260b5b
Rub21 <ruben@mapbox.com>
2014-12-01 16:26:43 -0500

    clone data

--- app/app.js
+++ app/app.js
@@ -8,13 +8,19 @@ var end_str = dates[2];
 var start_times = (new Date(start_str + " 00:00:00").getTime() / 1000);
 var end_times = new Date(end_str + " 00:00:00").getTime() / 1000 + 24 * 60 * 60;
 
+var json_way = null;
+var json_node = null;
+var json_relation = null;
 $.ajax({
     dataType: "json",
     url: host + type + '&' + start_times + '&' + end_times,
     success: function(json) {
-        draw_way(json);
-        draw_node(json);
-        draw_relation(json);
+        json_way = _.map(json, _.clone);
+        json_node = _.map(json, _.clone);
+        json_relation = _.map(json, _.clone);
+        draw_node(json_node);
+        draw_way(json_way);
+        draw_relation(json_relation);
         location.href = document.URL.split('#')[0] + '#' + type + '&' + start_str + '&' + end_str;
     }
 });
@@ -22,9 +28,9 @@ $.ajax({
 function draw_way(data) {
     var chart;
     var nv_way = nv;
+
     nv_way.addGraph(function() {
-        chart = nv.models.multiBarChart()
-            //.barColor(d3.scale.category20().range())
+        chart = nv_way.models.multiBarChart()
             .margin({
                 top: 50,
                 right: 20,
@@ -36,36 +42,19 @@ function draw_way(data) {
             .rotateLabels(0)
             .groupSpacing(0.1);
 
-        chart.multibar
-            .hideable(true);
-
+        chart.multibar.hideable(true);
         chart.reduceXTicks(false).staggerLabels(true);
-
-        /* chart.xAxis
-             .axisLabel("Hours")
-             .showMaxMin(true)
-             .tickFormat(d3.format(','));*/
-
         chart.xAxis.tickFormat(function(d) {
-            //2014-11-09 23
             return d;
-            //return d.split(' ')[0].substring(8, 11) + '-' + d.split(' ')[1] + 'h';
-            /*
-                        var dx = d[0].values[d] && d[0].values[d][0] || 0;
-                        return d3.time.format('%x')(new Date(dx))*/
         });
 
         chart.yAxis
             .tickFormat(d3.format(',.H'));
-
         d3.select('#chart_way svg')
             .datum(function() {
                 var json_way = [];
                 _.each(data, function(val, key) {
                     val.values = val.values_way;
-                    // val.values_node = null;
-                    // val.values_way = null;
-                    //val.values_relation = null;
                     json_way.push(val);
                 });
                 console.log(json_way)
@@ -73,18 +62,12 @@ function draw_way(data) {
 
             })
             .call(chart);
-
         nv.utils.windowResize(chart.update);
-
-        chart.dispatch.on('stateChange', function(e) {
-            nv.log('New State:', JSON.stringify(e));
-        });
-
-
+        console.log(chart);
         return chart;
+
     });
 }
-
 
 function draw_node(data) {
     var chart;
@@ -117,6 +100,8 @@ function draw_node(data) {
                 var json_node = [];
                 _.each(data, function(val, key) {
                     val.values = val.values_node;
+                    // val.values_node = null;
+
                     json_node.push(val);
                 });
 
@@ -126,21 +111,14 @@ function draw_node(data) {
 
             })
             .call(chart);
-
         nv.utils.windowResize(chart.update);
-
-        chart.dispatch.on('stateChange', function(e) {
-
-            console.log(e);
-            nv.log('New State:', JSON.stringify(e));
-        });
         return chart;
     });
 }
 
 function draw_relation(data) {
     var chart;
-    var nv_relation=nv;
+    var nv_relation = nv;
     nv_relation.addGraph(function() {
         chart = nv.models.multiBarChart()
             .margin({
@@ -168,16 +146,14 @@ function draw_relation(data) {
                 var json_relation = [];
                 _.each(data, function(val, key) {
                     val.values = val.values_relation;
+
+                    //val.values_relation = null;
                     json_relation.push(val);
                 });
                 return json_relation;
             })
             .call(chart);
         nv.utils.windowResize(chart.update);
-        chart.dispatch.on('stateChange', function(e) {
-            nv.log('New State:', JSON.stringify(e));
-
-        });
         return chart;
     });
 }
@@ -188,9 +164,7 @@ $(document).ready(function() {
     //1416096000 AND d.osmdate<1416182400
     //#h&2014-11-15&2014-11-16
     if (start_times > end_times) {
-        return null;
-    } else {
-        console.log('fine');
+        alert('Select a range of correct dates');
     }
     $('.date-picker').val(dates[1]);
     $('.date-picker1').val(dates[2]);
@@ -241,13 +215,17 @@ function draw() {
     end_str = $('.date-picker1').val();
     end_times = new Date(end_str + " 00:00:00").getTime() / 1000 + 24 * 60 * 60;
     console.log(host + type + '&' + start_times + '&' + end_times);
+
     $.ajax({
         dataType: "json",
         url: host + type + '&' + start_times + '&' + end_times,
         success: function(json) {
-            draw_way(json);
-            draw_node(json);
-            draw_relation(json);
+            json_way = _.map(json, _.clone);
+            json_node = _.map(json, _.clone);
+            json_relation = _.map(json, _.clone);
+            draw_node(json_node);
+            draw_way(json_way);
+            draw_relation(json_relation);
             location.href = document.URL.split('#')[0] + '#' + type + '&' + start_str + '&' + end_str;
         }
     });
