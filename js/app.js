848410cf45c03072498864e16126f50dae346db0
Rub21 <ruben@mapbox.com>
2014-12-01 22:09:49 -0500

    server

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-var host = 'http://localhost:3021/';
-//var host = 'http://54.172.162.212:3022/'
+//var host = 'http://localhost:3021/';
+var host = 'http://54.172.162.212:3022/'
 var type = 'd';
 var dates = document.URL.split('#')[1].split('&');
 type = dates[0];
