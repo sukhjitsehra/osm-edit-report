049e0b64b6cbf67c1232bc62b6f7ebc3042d0081
Rub21 <ruben@mapbox.com>
2015-02-01 10:10:57 -0500

    fix xAxis

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ var start_times = (new Date(start_str + " 00:00:00").getTime() / 1000);
 var end_times = new Date(end_str + " 00:00:00").getTime() / 1000 + 24 * 60 * 60;
 var months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 var json_obj = null;
-var date = [];
+var date_xaxis = [];
 var json_line = null;
 
 function draw_obj(data) {
@@ -57,8 +57,10 @@ function draw_obj(data) {
 }
 
 function draw_line(data) {
-    console.log(data);
     num_global_lenght = data[0].values.length;
+    global_parameters.d_type = type;
+    global_parameters.d_length = data[0].values.length;
+    //console.log(global_parameters);
     var chart;
     var nv_line = nv;
     nv_line.addGraph(function() {
@@ -66,17 +68,18 @@ function draw_line(data) {
         chart = nv_line.models.lineChart().useInteractiveGuideline(true);
         chart
             .x(function(d, i) {
+                //console.log(d.x);
                 return d.x;
             });
+
+        //
+
         var formatter;
-        date = _.uniq(date);
 
         switch (type) {
             case 'h':
                 //per hour
                 formatter = function(d, i) {
-                    //console.log(d);
-                    //Mon Jan 05 2015 12:00:00 GMT-0500 (PET)
                     if (typeof d === 'object') {
                         d = (d + "").split(' ');
                         return d[4].split(':')[0] + 'h' + '-' + d[2];
@@ -91,7 +94,10 @@ function draw_line(data) {
                 formatter = function(d, i) {
                     if (typeof d === 'object') {
                         d = d + "";
+                        //console.log(d.substr(4, 11));
                         return d.substr(4, 11);
+
+
                     } else {
                         var date = new Date(d);
                         return d3.time.format('%b %d %Y')(date);
@@ -128,17 +134,35 @@ function draw_line(data) {
             right: 63,
             left: 35
         });
-
         chart.xAxis
             .axisLabel('Date')
             .tickFormat(
                 formatter
             );
 
-        chart.xScale(d3.time.scale());
+        //reduce el numero de labels en el xAxis
+        date_xaxis = _.uniq(date_xaxis);
+        var date_xaxis2 = [];
 
-        chart.yAxis
-            .tickFormat(d3.format(',.2f'));
+         console.log(date_xaxis);
+
+        if (date_xaxis.length > 10) {
+            date_xaxis2 = _.each(date_xaxis, function(v, k) {
+                return k % 2 == 0;
+            });
+            date_xaxis = date_xaxis2;
+        }
+
+        console.log(date_xaxis);
+        chart.xAxis.tickValues(date_xaxis);
+         console.log(date_xaxis);
+        //chart.xAxis.ticks(4);
+        //chart.xDomain();
+        //chart.xAxis.ticks(2);
+        //chart.xScale(d3.time.scale());
+        //chart.xAxis.axisLabelDistance(400);
+
+        chart.yAxis.tickFormat(d3.format(',.2f'));
         d3.select('#chart_line svg')
             .datum(data)
             .transition().duration(500)
@@ -299,7 +323,7 @@ function draw() {
             case 'y':
                 start_str = $('.from').val();
                 start_times = Date.UTC(parseInt(start_str.split('-')[0]), 0, 1) / 1000;
-                console.log(start_times);
+                //console.log(start_times);
                 end_str = $('.to').val();
                 end_times = Date.UTC(parseInt(end_str.split('-')[0]), 1, 1) / 1000 + 24 * 60 * 60 * 365;
                 start_str = start_str.split('-')[0] + '-01-01';
@@ -337,8 +361,8 @@ function draw() {
             //draw_way(json_way);
             //draw_relation(json_relation);
             // draw_obj(json);
-            console.log(json);
-            date = [];
+            //console.log(json);
+            date_xaxis = [];
             json_line = [];
             _.each(json, function(val, key) {
                 val.values_obj = null;
@@ -359,12 +383,17 @@ function draw() {
                     case 'd':
                         //per day
                         _.each(val.values, function(v, k) {
+                            //console.log(v);
+                            //console.log(k);
                             var d = val.values[k].x.split('-');
                             var utc = new Date(Date.UTC(d[0],
                                 parseInt(d[1]) - 1,
                                 parseInt(d[2]) + 1, 0, 0));
                             //val.values[k].label = val.values[k].x;
                             val.values[k].x = utc;
+                            date_xaxis.push(Date.UTC(d[0],
+                                parseInt(d[1]) - 1,
+                                parseInt(d[2]) + 1, 0, 0));
 
                         });
                         break;
@@ -385,7 +414,7 @@ function draw() {
                             console.log(d);
 
                             var utc = new Date(Date.UTC(d[0], 0, 2, 0, 0));
-                            console.log(utc);
+                            // console.log(utc);
                             //val.values[k].label = val.values[k].x;
                             val.values[k].x = utc;
 
@@ -394,7 +423,7 @@ function draw() {
                 }
                 json_line.push(val);
             });
-            console.log(json_line);
+            //console.log(json_line);
             draw_line(json_line);
         }
     });
