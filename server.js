62f4d0ec854ee7c5449c3409f7d0548b376fc52a
Rub21 <ruben@mapbox.com>
2015-02-27 11:28:40 -0500

    add status 200

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 var express = require('express');
 var cors = require('cors');
 var app = express();
@@ -35,87 +36,102 @@ client.connect(function(err) {
 });
 app.get('/:date', function(req, res) {
 	try {
-		value_parameters(req.params.date);
-		var date = (req.params.date + '').split('&');
-		var array_objs = [];
-		var query_obj = {
-      		text: 'SELECT substring(to_timestamp(osmdate)::text,0,$1) as osm_date',
-      		values: [type[date[0]]]
-    	};
-		var query_user = {
-      		text: 'SELECT iduser, osmuser, color, estado FROM osm_user WHERE estado = $1',
-      		values: [true]
-    	};
-		var main_query = client.query(query_user, function(error, result) {
-			if (error) {
-				console.log('Error on list users');
-				res.statusCode = 404;
-				return res.send('Error 404: No quote found');
-			} else {
-				for (var i = 0; i < result.rows.length; i++) {
-					user = new obj();
-					var iduser = result.rows[i].iduser;
-					query_obj.text += ", SUM(u_" + iduser + ") as u_" + iduser;
-					user.iduser = iduser;
-					user.key = result.rows[i].osmuser;
-					user.color = '#' + result.rows[i].color;
-					array_objs.push(user);
-				}
-			}
-		});
-		main_query.on('end', function(result) {
-			query_obj.text += " FROM osm_obj WHERE osmdate >= $2 AND osmdate < $3 GROUP BY osm_date ORDER BY osm_date";
-			query_obj.values.push(parseInt(date[1]),parseInt(date[2]));			
-			client.query(query_obj, function(error, result) {
+		var value = value_parameters(req.params.date);
+		if (value === 0) {
+			res.statusCode = 404;
+			console.log('Error: Bad parameteres');
+			res.send('Error 404: Bad parameteres');
+			res.end();
+		} else if (value === 1) {
+			res.statusCode = 200;
+			console.log('Test status');
+			res.send('Successful status');
+			res.end();
+		} else {
+			var date = (req.params.date + '').split('&');
+			var array_objs = [];
+			var query_obj = {
+				text: 'SELECT substring(to_timestamp(osmdate)::text,0,$1) as osm_date',
+				values: [type[date[0]]]
+			};
+			var query_user = {
+				text: 'SELECT iduser, osmuser, color, estado FROM osm_user WHERE estado = $1',
+				values: [true]
+			};
+			var main_query = client.query(query_user, function(error, result) {
 				if (error) {
-					console.log('Error on request parameter: '+ date[1] +', '+date[2]);
+					console.log('Error on list users');
 					res.statusCode = 404;
-					return res.send('Error 404: No quote found');
+					res.send('Error 404: No quote found');
+					res.end();
 				} else {
 					for (var i = 0; i < result.rows.length; i++) {
-						_.each(array_objs, function(v, k) {
-							array_objs[k].values.push({
-									x: result.rows[i].osm_date.replace(' ', '-'),
-									y: parseInt(result.rows[i]["u_" + v.iduser])
-								}
-
-							)
-						});
+						user = new obj();
+						var iduser = result.rows[i].iduser;
+						query_obj.text += ", SUM(u_" + iduser + ") as u_" + iduser;
+						user.iduser = iduser;
+						user.key = result.rows[i].osmuser;
+						user.color = '#' + result.rows[i].color;
+						array_objs.push(user);
 					}
-					res.json(array_objs);
-					console.log('Successful');				
 				}
 			});
-		});
+			main_query.on('end', function(result) {
+				query_obj.text += " FROM osm_obj WHERE osmdate >= $2 AND osmdate < $3 GROUP BY osm_date ORDER BY osm_date";
+				query_obj.values.push(parseInt(date[1]), parseInt(date[2]));
+				client.query(query_obj, function(error, result) {
+					if (error) {
+						console.log('Error on request parameter: ' + date[1] + ', ' + date[2]);
+						res.statusCode = 404;
+						res.send('Error 404: No quote found');
+						res.end();
+					} else {
+						for (var i = 0; i < result.rows.length; i++) {
+							_.each(array_objs, function(v, k) {
+								array_objs[k].values.push({
+										x: result.rows[i].osm_date.replace(' ', '-'),
+										y: parseInt(result.rows[i]["u_" + v.iduser])
+									}
+
+								)
+							});
+						}
+						res.json(array_objs);
+						console.log('Successful');
+						res.end();
+					}
+				});
+			});
+		}
 	} catch (e) {
 		res.statusCode = 404;
-		return res.send('Error 404: No quote found');
+		res.send('Error 404: No quote found');
+		res.end();
 	}
 });
 app.listen(process.env.PORT || 3021);
 
 function value_parameters(date) {
 	console.log('Request Date : ' + new Date() + ' url: ' + url + date);
-	date = (date + '').split('&');
-	if (date.length !== 3) {
-		console.log('error');
-		res.statusCode = 404;
-		return res.send('Error 404: No quote found');
+	var status = 2;
+	if (date === 'status') {
+		status = 1;
+	} else {
+		date = (date + '').split('&');
+		if (date.length !== 3) {
+			status = 0;
+		}
+		if (Number(date[1]) === NaN || Number(date[2]) === NaN || date[1].match(/^[0-9]+$/) === null || date[2].match(/^[0-9]+$/) === null || type[date[0]] === undefined) {
+
+			status = 0;
+		}
+		if (parseInt(date[2]) - parseInt(date[1]) < 0) {
+			status = 0;
+		}
+		//we can do a consult per hour less two months
+		if (parseInt(date[2]) - parseInt(date[1]) > 60 * 24 * 3600 && date[0] === 'h') {
+			status = 0;
+		}
 	}
-	if (Number(date[1]) === NaN || Number(date[2]) === NaN || type[date[0]] === undefined) {
-		console.log('error');
-		res.statusCode = 404;
-		return res.send('Error 404: No quote found');
-	}
-	if (parseInt(date[2]) - parseInt(date[1]) < 0) {
-		console.log('error');
-		res.statusCode = 404;
-		return res.send('Error 404: No quote found');
-	}
-	//we can do a consult per hour less two months
-	if (parseInt(date[2]) - parseInt(date[1]) > 60 * 24 * 3600 && date[0] === 'h') {
-		console.log('High petición of data per hour ');
-		res.statusCode = 404;
-		return res.send('Error 404: No quote found');
-	}
+	return status;
 }
