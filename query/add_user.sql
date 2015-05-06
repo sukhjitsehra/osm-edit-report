ef0d620d48f1d6945307034bf29cfcc305f166a3
Rub21 <ruben@mapbox.com>
2015-05-07 00:12:43 +0530

    graficos

--- query/add_user.sql
+++ query/add_user.sql
@@ -5,16 +5,15 @@ select add_user(2219338,'RichRico','EE3344',true);
 select add_user(2226712,'dannykath','662289',true);
 select add_user(94578,'andygol','3E8380',true);
 select add_user(1051550,'shravan91','FF8A00',true);
-select add_user(2554698,'Ruth Maben','FA58F4',true);
+select add_user(2554698,'ruthmaben','FA58F4',true);
 select add_user(2377377,'abel801','008000',true);
 select add_user(2511706,'calfarome','800000',true);
 select add_user(2512300,'samely','66CCCC',true);
 select add_user(2115749,'srividya_c','7B68EE',true);
-select add_user(1306,'PlaneMad','B22222',true);
+select add_user(1306,'PlaneMad','ADFF2F',true);
 select add_user(2748195,'karitotp','00BFFF',true);
 select add_user(2644101,'Chetan_Gowda','336699',true);
-select add_user(2823295,'Ramya Ragupathy','996633',true);
+select add_user(2823295,'ramyaragupathy','996633',true);
 select add_user(29936237,'ksharma198','00CC99 ',true);
+SELECT add_user(589596,'lxbarth','333366',true);
 SELECT add_user(2306749,'shvrm','FF00FF',true);
-
-
