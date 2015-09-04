6a905f306d7acbb93ea4a9454e3b94f790e8e1cc
Ruben LM <ruben@mapbox.com>
2015-09-04 05:48:50 -0500

    change oini color

--- query/add_user.sql
+++ query/add_user.sql
@@ -21,7 +21,7 @@ SELECT add_user(2905914,'pratikyadav','00f2ff',true);
 SELECT add_user(2847988,'jinalfoflia','ff6a6a',true);
 SELECT add_user(2835928,'Nikhil Prabhakar U','FF8A00',true);
 SELECT add_user(2985232,'aarthy','666699',true);
-SELECT add_user(3057995,'oini','669999',true);
+SELECT add_user(3057995,'oini','FFCC99',true);
 
 --UPDATE osm_user
 --   SET color= '666600'
