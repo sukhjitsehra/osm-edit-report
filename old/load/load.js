41d802564f76637c4e78d8d96567ee0cb6843d22
Aruna S <aruna@mapbox.com>
2015-12-07 15:39:47 +0530

    Add old OSM edit report code in its own folder

---
+++ old/load/load.js
@@ -0,0 +1,106 @@
+'use strict'
+
+var osmium = require('osmium');
+var fs = require('fs');
+var zlib = require('zlib');
+var _ = require('underscore');
+var argv = require('optimist').argv;
+var pg = require('pg');
+var download = require('./src/download');
+var database = require('./src/database');
+var count = require('./src/count');
+var removefiles = require('./src/removefiles');
+var crontab = require('node-crontab');
+// Initialize parameters
+var flag = true;
+
+
+var db_conf = {
+	pguser: argv.user,
+	pgpassword: argv.password,
+	pghost: argv.host,
+	pgdatabase: argv.database
+};
+
+
+var client = new pg.Client(
+	"postgres://" + (db_conf.pguser || 'postgres') +
+	":" + (db_conf.pgpassword || '1234') +
+	"@" + (db_conf.pghost || 'localhost') +
+	"/" + (db_conf.pgdatabase || 'dbstatistic')
+);
+client.connect(function(err) {
+	if (err) {
+		return console.error('could not connect to postgres', err);
+	}
+});
+
+
+var number_obj = {
+	prev: [],
+	current: [parseInt(argv.num_directory), parseInt(argv.num_file)],
+	next: []
+};
+
+var runSpeed = 10 * 1000;
+
+//inicio
+function init() {
+	download(number_obj.current, select_users);
+}
+
+//fecth users
+function select_users(b) {
+	if (b) {
+		database.select_users(client, process_files);
+	} else {
+		if (flag) {
+			flag = false;
+			crontab.scheduleJob("*/5 * * * *", function() { //This will call this function every 2 minutes 
+				init();
+			});
+		}
+	}
+}
+
+//count number of edition
+function process_files(users) {
+	count(number_obj.current, users, save);
+}
+
+//save on db
+function save(obj) {
+	database.insert(client, obj);
+	//filter(number_obj.current, obj, finish)
+	//remove files 
+	removefiles(number_obj.current);
+	console.log("fin");
+	get_num(number_obj.next);
+	init();
+
+}
+
+function get_num(arr) {
+	number_obj = {
+		prev: [],
+		current: arr,
+		next: []
+	};
+	if (arr[1] === 0) {
+		number_obj.prev.push(arr[0] - 1);
+		number_obj.prev.push(999);
+	} else {
+		number_obj.prev.push(arr[0]);
+		number_obj.prev.push(arr[1] - 1);
+	}
+	if (arr[1] === 999) {
+		number_obj.next.push(arr[0] + 1);
+		number_obj.next.push(0);
+	} else {
+		number_obj.next.push(arr[0]);
+		number_obj.next.push(arr[1] + 1);
+	}
+}
+
+get_num(number_obj.current);
+init();
---
+++ old/query/tables.sql
@@ -0,0 +1,54 @@
+﻿--DROP TABLE osm_user
+--DROP TABLE osm_obj
+CREATE TABLE osm_user(
+	iduser INTEGER NOT NULL PRIMARY KEY,
+	osmuser varchar(50),
+	color varchar(6),
+	estado boolean not null
+);
+
+CREATE TABLE osm_obj(
+	osmdate INTEGER NOT NULL PRIMARY KEY
+);
+
+CREATE OR REPLACE FUNCTION addcol_user(_columname varchar)
+RETURNS VOID
+AS $$
+declare 
+    _flag varchar ;
+    _uo varchar;
+    _uc varchar;
+begin 
+	_uo = 'uo_'||_columname;
+	_uc = 'uc_'||_columname;
+	_flag = (SELECT attname FROM pg_attribute WHERE attrelid = (SELECT oid FROM pg_class WHERE relname = 'osm_obj') AND attname = _uo);
+	if(_flag is null ) then 
+		EXECUTE 'ALTER TABLE osm_obj ADD COLUMN ' || _uo || ' SMALLINT;';
+		EXECUTE 'ALTER TABLE osm_obj ADD COLUMN ' || _uc || ' SMALLINT;';
+		_flag = _columname ||' Was created';
+	else
+		_flag = _columname ||' Already exist';
+	end if;
+
+end;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE FUNCTION add_user( _iduser integer, _osmuser varchar, _color varchar, _estado boolean)
+returns varchar 
+AS $$
+declare 
+    _user varchar ;
+    _flag varchar; 
+begin 
+	_user = (SELECT _osmuser FROM osm_user WHERE iduser = _iduser);
+	if(_user is null ) then 
+		INSERT INTO osm_user(iduser, osmuser, color, estado) VALUES (_iduser, _osmuser, _color, _estado);
+		PERFORM addcol_user(_iduser::text);
+		_flag = 'User ' || _osmuser ||' Was created';
+	else
+		_flag = 'User ' || _user ||' Already exist';
+	end if;
+return _flag;
+end;
+$$ LANGUAGE plpgsql;
